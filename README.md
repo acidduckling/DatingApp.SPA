19aa6970e9892a561c65808a8509c352189b78c1
Clayton Wakefield <acidduckling@gmail.com>
2018-04-03 10:28:58 +1000

    Added 3rd party components

--- README.md
+++ README.md
@@ -25,3 +25,11 @@ Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protrac
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+
+
+## NOTES
+### Third Party Libraries
+- Alerts: http://alertifyjs.com
+- Angular JWT: https://github.com/auth0/angular2-jwt
+- Angular Bootstrap: https://valor-software.com/ngx-bootstrap
+- Custom Bootstrap theme: https://bootswatch.com/
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AuthService } from './../_services/auth.service';
+import { AlertifyService } from './../_services/alertify.service';
 
 @Component({
   selector: 'app-register',
@@ -10,21 +11,20 @@ export class RegisterComponent implements OnInit {
   model: any = {};
   @Output() cancelRegister = new EventEmitter();
 
-  constructor(private AuthService: AuthService) { }
+  constructor(private authService: AuthService, private alertify: AlertifyService ) { }
 
   ngOnInit() {
   }
 
   register() {
-    this.AuthService.register(this.model).subscribe(() => {
-      console.log('registration successful');
+    this.authService.register(this.model).subscribe(() => {
+      this.alertify.success('registration successful');
     }, error => {
-      console.log(error);
+      this.alertify.error(error);
     });
   }
 
   cancel() {
     this.cancelRegister.emit(false);
-    console.log('cancelled');
   }
 }
